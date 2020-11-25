2778506039d2470f4058e22d89140657cf9ac460
Vanessa Martin <vanessa.s.martin96@gmail.com>
2020-11-25 08:26:38 -0800

    pod landing page - v1

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const alert = document.getElementById("error");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validateInput();
+});
+
+function validateInput() {
+  const emailVal = email.value.trim();
+  if (emailVal === "") {
+    displayError("fail", "Email cannot be blank!");
+    return;
+  } else if (!isValid(emailVal)) {
+    displayError("fail", "Invalid email! Try again");
+    return;
+  } else {
+    displayError("success", "Email received!");
+  }
+}
+
+function displayError(type, message) {
+  alert.style.display = "block";
+  alert.style.margin = ".75rem";
+  let alertColor;
+  if (type === "fail") {
+    alertColor = "red";
+  } else {
+    alertColor = "green";
+  }
+  alert.style.color = alertColor;
+  alert.innerText = message;
+}
+
+function isValid(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
